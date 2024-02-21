cb7ff3986468c05982e9af1d91a0a8b21e630640
joaomanoel <belezajovaniatb@gmail.com>
2024-02-21 14:00:08 -0300

    chore(structs): added if_struct

--- src/structs.rs
+++ src/structs.rs
@@ -1,16 +1,11 @@
 pub mod binary;
 pub mod bool;
-pub mod call;
 pub mod first;
-pub mod function;
 pub mod if_struct;
 pub mod int;
-pub mod let_struct;
 pub mod location;
-pub mod parameter;
 pub mod print;
 pub mod second;
 pub mod str;
 pub mod syntax_error;
 pub mod tuple;
-pub mod var;
