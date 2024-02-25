01c4fc8cc6dc477603a77b8e51d3eba47bbd8853
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:41:32 -0300

    chore(binary_case): added scope

--- src/use_cases/binary_case.rs
+++ src/use_cases/binary_case.rs
@@ -2,55 +2,23 @@ use crate::{
     enums::{binary_op::BinaryOp, return_types::ReturnTypes},
     eval,
     structs::{binary::Binary, syntax_error::new_syntax_error},
+    Scope,
 };
 
-pub fn binary_case(binary: Binary) -> ReturnTypes {
-    let eval_lhs = eval(*binary.lhs);
-    let eval_rhs = eval(*binary.rhs);
-
-    match (eval_lhs, eval_rhs) {
+pub fn binary_case(binary: Binary, scope: &Scope) -> ReturnTypes {
+    match (eval(*binary.lhs, scope), eval(*binary.rhs, scope)) {
         (ReturnTypes::Int(a), ReturnTypes::Int(b)) => match binary.op {
             BinaryOp::Add => ReturnTypes::Int(a + b),
             BinaryOp::Sub => ReturnTypes::Int(a - b),
             BinaryOp::Mul => ReturnTypes::Int(a * b),
             BinaryOp::Div => ReturnTypes::Int(a / b),
             BinaryOp::Rem => ReturnTypes::Int(a % b),
-            BinaryOp::Eq => {
-                if a == b {
-                    return ReturnTypes::Bool(true);
-                };
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Neq => {
-                if a != b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Lt => {
-                if a < b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Gt => {
-                if a > b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Lte => {
-                if a <= b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Gte => {
-                if a >= b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
+            BinaryOp::Eq => ReturnTypes::Bool(a == b),
+            BinaryOp::Neq => ReturnTypes::Bool(a != b),
+            BinaryOp::Lt => ReturnTypes::Bool(a < b),
+            BinaryOp::Gt => ReturnTypes::Bool(a > b),
+            BinaryOp::Lte => ReturnTypes::Bool(a <= b),
+            BinaryOp::Gte => ReturnTypes::Bool(a >= b),
             _ => new_syntax_error(binary.location),
         },
         (ReturnTypes::Int(a), ReturnTypes::Str(b)) => match binary.op {
@@ -59,49 +27,19 @@ pub fn binary_case(binary: Binary) -> ReturnTypes {
         },
         (ReturnTypes::Str(a), ReturnTypes::Str(b)) => match binary.op {
             BinaryOp::Add => ReturnTypes::Str(a + &b),
-            BinaryOp::Eq => {
-                if a == b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Neq => {
-                if a != b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
+            BinaryOp::Eq => ReturnTypes::Bool(a == b),
+            BinaryOp::Neq => ReturnTypes::Bool(a != b),
             _ => new_syntax_error(binary.location),
         },
         (ReturnTypes::Str(a), ReturnTypes::Int(b)) => match binary.op {
             BinaryOp::Add => ReturnTypes::Str(a + &b.to_string()),
-            _ => panic!("Syntax Error"),
+            _ => new_syntax_error(binary.location),
         },
         (ReturnTypes::Bool(a), ReturnTypes::Bool(b)) => match binary.op {
-            BinaryOp::Eq => {
-                if a == b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Neq => {
-                if a != b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::And => {
-                if a && b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
-            BinaryOp::Or => {
-                if a || b {
-                    return ReturnTypes::Bool(true);
-                }
-                ReturnTypes::Bool(false)
-            }
+            BinaryOp::Eq => ReturnTypes::Bool(a == b),
+            BinaryOp::Neq => ReturnTypes::Bool(a != b),
+            BinaryOp::And => ReturnTypes::Bool(a && b),
+            BinaryOp::Or => ReturnTypes::Bool(a || b),
             _ => new_syntax_error(binary.location),
         },
         _ => new_syntax_error(binary.location),
