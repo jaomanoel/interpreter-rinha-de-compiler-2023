0bc728fccac43029572b5179e9af61d432e7c899
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:37:08 -0300

    chore(term): added let, var, function, and call

--- src/enums/term.rs
+++ src/enums/term.rs
@@ -1,11 +1,11 @@
 use serde::Deserialize;
 
 use crate::structs::{
-    binary::Binary, bool::Bool, first::First, if_struct::If, int::Int, print::Print,
-    second::Second, str::Str, tuple::Tuple,
+    binary::Binary, bool::Bool, call::Call, first::First, function::Function, if_struct::If,
+    int::Int, let_struct::Let, print::Print, second::Second, str::Str, tuple::Tuple, var::Var,
 };
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 #[serde(tag = "kind")]
 pub enum Term {
     Int(Int),
@@ -17,4 +17,8 @@ pub enum Term {
     First(First),
     Second(Second),
     If(If),
+    Function(Function),
+    Let(Let),
+    Var(Var),
+    Call(Call),
 }
