80a78fc8fdcc908270b4780f06dbe15903c46cac
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:21:12 -0300

    chore(first): added clone and partialEq

--- src/structs/first.rs
+++ src/structs/first.rs
@@ -2,7 +2,7 @@ use super::location::Location;
 use crate::enums::term::Term;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 pub struct First {
     pub value: Box<Term>,
     pub location: Location,
