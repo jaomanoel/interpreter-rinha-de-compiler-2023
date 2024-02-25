cacb6b3d1d74f566b68dbabd6430a474f3d94f1e
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:42:15 -0300

    chore(if_case): added scope

--- src/use_cases/if_case.rs
+++ src/use_cases/if_case.rs
@@ -2,14 +2,15 @@ use crate::{
     enums::{return_types::ReturnTypes, term::Term},
     eval,
     structs::{if_struct::If, syntax_error::new_syntax_error},
+    Scope,
 };
 
-pub fn if_case(if_struct: If) -> ReturnTypes {
+pub fn if_case(if_struct: If, scope: &Scope) -> ReturnTypes {
     let condition = *if_struct.condition;
 
     match condition {
-        Term::Bool(_) => if_case_execute(condition, *if_struct.then, *if_struct.otherwise),
-        Term::Binary(_) => if_case_execute(condition, *if_struct.then, *if_struct.otherwise),
+        Term::Bool(_) => if_case_execute(condition, *if_struct.then, *if_struct.otherwise, scope),
+        Term::Binary(_) => if_case_execute(condition, *if_struct.then, *if_struct.otherwise, scope),
         Term::Print(print) => new_syntax_error(print.location),
         Term::Str(str) => new_syntax_error(str.location),
         Term::First(first) => new_syntax_error(first.location),
@@ -17,16 +18,20 @@ pub fn if_case(if_struct: If) -> ReturnTypes {
         Term::If(if_s) => new_syntax_error(if_s.location),
         Term::Int(int) => new_syntax_error(int.location),
         Term::Tuple(tuple) => new_syntax_error(tuple.location),
+        Term::Let(let_s) => new_syntax_error(let_s.location),
+        Term::Function(function) => new_syntax_error(function.location),
+        Term::Var(var) => new_syntax_error(var.location),
+        Term::Call(call) => new_syntax_error(call.location),
     }
 }
 
-fn if_case_execute(condition: Term, then: Term, otherwise: Term) -> ReturnTypes {
-    match eval(condition) {
+fn if_case_execute(condition: Term, then: Term, otherwise: Term, scope: &Scope) -> ReturnTypes {
+    match eval(condition, scope) {
         ReturnTypes::Bool(value) => {
             if value {
-                eval(then)
+                eval(then, scope)
             } else {
-                eval(otherwise)
+                eval(otherwise, scope)
             }
         }
         _ => ReturnTypes::Void,
