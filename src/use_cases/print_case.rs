9ed92e63f504b29588fa4a9197360a4e8cbf8a27
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:42:58 -0300

    chore(print_case): added scope

--- src/use_cases/print_case.rs
+++ src/use_cases/print_case.rs
@@ -2,12 +2,11 @@ use crate::{
     enums::return_types::ReturnTypes,
     eval,
     structs::{print::Print, syntax_error::new_syntax_error},
+    Scope,
 };
 
-pub fn print_case(print: Print) -> ReturnTypes {
-    let eval = eval(*print.value);
-
-    match eval {
+pub fn print_case(print: Print, scope: &Scope) -> ReturnTypes {
+    match eval(*print.value, scope) {
         ReturnTypes::Str(s) => {
             println!("{s}");
             ReturnTypes::Void
@@ -18,6 +17,10 @@ pub fn print_case(print: Print) -> ReturnTypes {
         }
         ReturnTypes::Bool(b) => {
             println!("{b}");
+            ReturnTypes::Void
+        }
+        ReturnTypes::Tuple(tuple) => {
+            println!("{tuple:?}");
             ReturnTypes::Void
         }
         _ => new_syntax_error(print.location),
