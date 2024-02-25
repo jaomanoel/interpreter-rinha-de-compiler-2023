79b95fb9f9ac1177f18104c5c20b58ad8235596d
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:20:21 -0300

    chore(bool): added clone and partialEq

--- src/structs/bool.rs
+++ src/structs/bool.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 use super::location::Location;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 pub struct Bool {
     pub value: bool,
     pub location: Location,
