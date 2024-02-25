49f3a739d066a83ee1336dfb2eb46fc7260a49ae
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:32:11 -0300

    chore(return_types): added closure and call

--- src/enums/return_types.rs
+++ src/enums/return_types.rs
@@ -1,10 +1,12 @@
-use serde::Deserialize;
+use crate::structs::{call::Call, closure::Closure};
 
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum ReturnTypes {
     Void,
     Str(String),
     Int(i32),
     Bool(bool),
     Tuple((Box<ReturnTypes>, Box<ReturnTypes>)),
+    Closure(Closure),
+    Call(Call),
 }
