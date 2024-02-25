5ff7ee2a0baf183f4d72b9045aaf22097ba05fcb
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:25:45 -0300

    chore(tuple): added clone and partialEq

--- src/structs/tuple.rs
+++ src/structs/tuple.rs
@@ -2,7 +2,7 @@ use super::location::Location;
 use crate::enums::term::Term;
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, Clone)]
 pub struct Tuple {
     pub first: Box<Term>,
     pub second: Box<Term>,
