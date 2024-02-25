f7911b8c7f960529f2d8dc9723ed131df28bab56
joaomanoel <belezajovaniatb@gmail.com>
2024-02-25 11:34:52 -0300

    chore(structs): added let_struct, closure, function, parameter, call and var

--- src/structs.rs
+++ src/structs.rs
@@ -1,11 +1,17 @@
 pub mod binary;
 pub mod bool;
+pub mod call;
+pub mod closure;
 pub mod first;
+pub mod function;
 pub mod if_struct;
 pub mod int;
+pub mod let_struct;
 pub mod location;
+pub mod parameter;
 pub mod print;
 pub mod second;
 pub mod str;
 pub mod syntax_error;
 pub mod tuple;
+pub mod var;
